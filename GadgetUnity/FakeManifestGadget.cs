5cff97a7b1e3a471aba4d59fd293c12115628587
lassombra <briatt22@gmail.com>
2024-12-17 23:06:52 -0600

    Add unity components

---
+++ GadgetUnity/FakeManifestGadget.cs
@@ -0,0 +1,17 @@
+﻿using custom_item_components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+
+namespace GadgetUnity
+{
+    public class FakeManifestGadget : GadgetBase
+    {
+        public TextMeshPro header;
+        public TextMeshPro body;
+        public TextMeshPro train;
+    }
+}
---
+++ GadgetUnity/ItemSaveMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace GadgetUnity
+{
+    public class ItemSaveMapping : MonoBehaviour
+    {
+        public TextMeshPro header;
+        public TextMeshPro body;
+        public TextMeshPro train;
+    }
+}
